5346aeed8464ade68e2f3b80e7bc436e933e9b7d
pandaachu <pandaachu@gmail.com>
2020-11-24 14:31:15 +0800

    Update 2020-11-24T06:31:09.825Z

--- js/all.js
+++ js/all.js
@@ -31,7 +31,5 @@ $(".CLA-btn-try-btn").click(function () {
   $("html,body").animate({
     scrollTop: $(_go).offset().top
   }, 2000);
-}); //表單
-
-$(".formfield.answer7").prepend('<span class="checkbox_span"></span>'), $(".formfield.name input").attr("placeholder", "請輸入姓名"), $(".formfield.mobile input").attr("placeholder", "請輸入手機號碼"), $(".formfield.formfield_email input").attr("placeholder", "請輸入電子信箱"), $(".formfield.answer7 [for=dwfrm_oto_oevent_answer7]").html('提醒您送出資料後，即代表您同意將此資料提供予Lancôme 蘭蔻進行行銷處理與使用<a href="http://www.lancome.com.tw/customer-service-security-policy.html" target="_blank">[詳細個人資料權益說明]</a>'), $(".formfield.formfield_button #otoo-event-submit-button span").text("立即體驗");
+});
 //# sourceMappingURL=all.js.map
